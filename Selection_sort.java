1816bb08ed5c4e6b64197f89da7da62d0a67df5f
Senbagavalli N <142738030+senbagavalli04@users.noreply.github.com>
2025-01-18 19:06:59 +0530

    Create Selection_sort.java

---
+++ Selection_sort.java
@@ -0,0 +1,20 @@
+//CODE LINK : https://www.geeksforgeeks.org/problems/selection-sort/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+
+class Solution {
+    void selectionSort(int[] arr) {
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            int min = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[min] > arr[j]){
+                min = j;
+            }
+        }
+         int temp = arr[i];
+         arr[i] = arr[min];
+         arr[min] = temp;
+        }
+        
+    }
+}
